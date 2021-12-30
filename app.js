89c9a4335fea22ea511f79b6335c076bf506f48b
Ravindra <ravindralodhi987@gmail.com>
2021-12-31 00:53:59 +0530

    responding to URL parameter tutorial 54

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
  * middelware for data available in post api
  * middelware just a function that convert the incomming request data
  */
- 
+
 app.use(express.json());
 
 
@@ -32,11 +32,20 @@ const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simpl
 
 //Create get api for v1 and endpoint is tours
 
-app.get('/api/v1/tours',(req,res) =>{
+app.get('/api/v1/tours/:id', (req, res) => {
+    const ID = req.params.id * 1;
+    const tour = tours.find((ele) => ele.id === ID);
+    if (!tour) {
+        return res.status(404)
+            .json({
+                status: 'faild',
+                messege: "Invalid ID"
+            })
+    }
+
     res.status(200)
        .json({
            status : 'success',
-           results : tours.length,
            data :{
                tours
            }
@@ -45,29 +54,29 @@ app.get('/api/v1/tours',(req,res) =>{
 
 //Create post api for v1 and endpoint is tours to make data available in post the we have to use middelware
 
-app.post('/api/v1/tours',(req,res) => {
-//   console.log(req.body);
- const id = tours[tours.length - 1].id + 1;
- console.log(id);
- const newTour = Object.assign({id:id},req.body);
- console.log(newTour);
- tours.push(newTour);
- fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours),err =>{
-     if(!err){
-         res.status(200)
-            .json({
-                status : 'success',
-                data : {
-                    tour : newTour
-                }
-            })
-     }else{
-         console.log(err);
-     }
- })
+app.post('/api/v1/tours', (req, res) => {
+    //   console.log(req.body);
+    const id = tours[tours.length - 1].id + 1;
+    console.log(id);
+    const newTour = Object.assign({ id: id }, req.body);
+    console.log(newTour);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        if (!err) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: {
+                        tour: newTour
+                    }
+                })
+        } else {
+            console.log(err);
+        }
+    })
 })
 
 const port = 8080;
-app.listen(port,()=>{
-  console.log(`Server is running on port ${port}.`);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}.`);
 })
